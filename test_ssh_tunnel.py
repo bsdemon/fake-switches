d6994bf5b1438b244ac4d1037bc4d55f77506d23
Iliya Iliev <iliya.iliev@x3me.net>
2024-09-19 16:46:03 +0300

    Working ssh example

--- test_ssh_tunnel.py
+++ test_ssh_tunnel.py
@@ -1,16 +1,26 @@
+
+import io
+import paramiko
 from sshtunnel import SSHTunnelForwarder
 
+with open('id_rsa_fake', 'r') as file:
+    proxy_ssh_key = file.read().rstrip()
+    
+
+private_key = paramiko.RSAKey.from_private_key(
+    io.StringIO(proxy_ssh_key)
+)
+    
 server = SSHTunnelForwarder(
     ("localhost", 22222),
     ssh_username="root",
-    ssh_pkey="/home/eon/workspace/fake-switches/id_rsa_fake",
-    remote_bind_address=('192.168.1.101', 23),
+    ssh_pkey=private_key,
+    remote_bind_address=('192.168.27.101', 23),
     local_bind_address=("127.0.0.1", 10023)
 )
 
 server.start()
 
 print(server.local_bind_port)  # show assigned local port
-# work with `SECRET SERVICE` through `server.local_bind_port`.
 
 server.stop()
